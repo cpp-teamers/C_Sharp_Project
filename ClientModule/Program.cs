b5035e87bbfddf626642becc4fd676d14d92f37e
Саня FPV <andrei.cazanov2017@gmail.com>
2020-12-16 20:49:44 +0300

    Исходная версия кода

---
+++ ClientModule/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClientModule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DirectorModule/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DirectorModule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
