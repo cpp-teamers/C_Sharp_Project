892e77265f30670a459925dbb6f519c03f902431
Саня FPV <andrei.cazanov2017@gmail.com>
2020-12-20 20:36:47 +0300

    Начал работать с ClientMenu

---
+++ LibraryMenu/ClientMenu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Library1;
+
+public class ClientMenu
+{
+    public void StartMenu()
+    {
+        Console.WriteLine("╒════════════════════════════════════════════╕");
+        Console.WriteLine("│       We welcome you to the Happy_PC       │");
+        Console.WriteLine("│┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈│");
+        Console.WriteLine("│             1. Registration                │");
+        Console.WriteLine("│             2. Login in                    │");
+        Console.WriteLine("│             3. Show price list             │");
+        Console.WriteLine("╘════════════════════════════════════════════╛");
+    }
+    public void LogInMenu(Client client)
+    {
+        Console.WriteLine("╒════════════════════════════════════════════╕");
+       Console.WriteLine($"│         Welcome, {client.Name}!            │");
+        Console.WriteLine("│┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈│");
+        Console.WriteLine("│           1. Create new form               │");
+        Console.WriteLine("│           2. Show all my orders            │");
+        Console.WriteLine("│           3. Log out                       │");
+        Console.WriteLine("╘════════════════════════════════════════════╛");
+    }
+}
