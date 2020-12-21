710be3e41d2a272acea5e92a3a8323292aa45ba7
KhomenkoMac <m.khomenko03@gmail.com>
2020-12-21 23:25:31 +0200

    Start writing DataReapairMan Manager

--- RepairManModule/RepairManDataManager.cs
+++ RepairManModule/RepairManDataManager.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml.Serialization;
+using System.IO;
 
 namespace RepairManModule
 {
     class RepairManDataManager
     {
+
+
     }
 }
