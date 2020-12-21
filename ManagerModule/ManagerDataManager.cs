542fe1c6a0bf7ae6cabb6553fc4bb31fb27c6939
MrZend <vlad04102002@gmail.com>
2020-12-22 01:10:28 +0200

    System of authorised done it

--- ManagerModule/ManagerDataManager.cs
+++ ManagerModule/ManagerDataManager.cs
@@ -1,12 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
+using System.IO;
+using Library1;
 
 namespace ManagerModule
 {
     class ManagerDataManager
     {
-        
-    
+        private string path_managers = @"..\..\..\..\Data\menegers\";
+        private string path_clients = @"..\..\..\..\Data\clients\";
+        private BinaryFormatter bf = new BinaryFormatter();
+
+        public bool PasswordIsCorrect(string login, string password)
+        {
+            string path = path_clients + login + @"\" + "data.dat";
+            Manager manager = null;
+            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                manager = (Manager)bf.Deserialize(fs);
+            }
+            return (password == manager.AccountData.Password);
+        }
+
+        public Manager GetManager(string login)
+        {
+            string path = path_clients + login + @"\" + "data.dat";
+            Manager manager = null;
+            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                manager = (Manager)bf.Deserialize(fs);
+            }
+            return manager;
+        }
+
+        public List<Order> GetNotDistributedOrders(string login) // достроить логику
+        {
+            DirectoryInfo di = new DirectoryInfo(path_clients);
+            FileInfo[] files = di.GetFiles("[^0-9]*.dat");
+
+            List<Order> orders = new List<Order>();
+
+            foreach(FileInfo file in files)
+            {
+                string path = path_clients + @"\n" + file.FullName;
+            }
+            return orders;
+        }
     }
 }
--- ManagerModule/Program.cs
+++ ManagerModule/Program.cs
@@ -2,6 +2,7 @@
 using LibraryMenu;
 using LibraryDialog;
 using System.IO;
+using Library1;
 
 namespace ManagerModule
 {
@@ -11,26 +12,48 @@ namespace ManagerModule
         {
             ManagerMenu menu = new ManagerMenu();
             ManagerDialog dialog = new ManagerDialog();
-            
+            ManagerDataManager dataManager = new ManagerDataManager();
+            ManagerMenu.exit = true;
+
             do
             {
-                ManagerMenu.exit = true;
                 menu.StartMenu();
                 switch (dialog.ChoosenParametr())
                 {
                     case 1:
+                        ManagerMenu.counterOfIncorrect = 0;
                         do
                         {
                             string login = dialog.LogInDialog();
-                            if (Directory.Exists(@$"..\..\..\..\Data\managers\{login}"))
+                            if (Directory.Exists(@$"..\..\..\..\Data\managers\{login}") && dataManager.PasswordIsCorrect(login, dialog.PasswordDialog()))
                             {
+                                Manager manager = dataManager.GetManager(login);
+                                do
+                                {
+                                    menu.LogInMenu(manager);
+                                    switch (dialog.ChoosenParametr())
+                                    {
+                                        case 1:
 
+                                            break;
+                                        case 2:
+                                            break;
+                                        case 3:
+                                            ManagerMenu.exit = false;
+                                            break;
+                                        default:
+                                            Console.WriteLine("!UNKNOWN PARAMS!");
+                                            break;
+                                    }
+
+                                } while (ManagerMenu.exit && dialog.AllowContinue());
                             }
                             else
                             {
-
+                                Console.WriteLine($"\n> Login or password is incorrect!!! Left {3 - ManagerMenu.counterOfIncorrect} try");
+                                ManagerMenu.counterOfIncorrect++;
                             }
-                        } while()
+                        } while (ManagerMenu.counterOfIncorrect < 3 && ManagerMenu.exit);
                         break;
                     case 2:
                         ManagerMenu.exit = false;
