7d87cf0e6c0fa2000fde365316b3a158889cdd40
Саня FPV <andrei.cazanov2017@gmail.com>
2020-12-21 15:01:25 +0300

    Создал библиотеку LibraryDialog, доработка ClientModule

---
+++ LibraryDialog/ClientDialog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryDialog
+{
+    public class ClientDialog
+    {
+        /*public char AllowContinue()
+        {
+            try
+            {
+            }
+
+        }*/
+    }
+}
