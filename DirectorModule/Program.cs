2d2e956b837f0f99d25a4f7fd5eb92ec86b9fea8
Саня FPV <andrei.cazanov2017@gmail.com>
2020-12-23 14:43:48 +0300

    Начал работу DirectorModule

--- DirectorModule/Program.cs
+++ DirectorModule/Program.cs
@@ -1,12 +1,83 @@
 ﻿using System;
-
+using LibraryMenu;
+using LibraryDialog;
 namespace DirectorModule
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.SetWindowSize(80, 50);
+            DirectorMenu directorMenu = new DirectorMenu();
+            DirectorDialog directorDialog = new DirectorDialog();
+            DirectorDataManager dataManager = new DirectorDataManager();
+            Director director = new Director();
+            // bool
+            bool exit = false;
+            int safer = 3; // 3 попытки ввести верный догин и пароль
+            do
+            {
+                Console.Clear();
+
+                directorMenu.StartMenu();
+                switch (directorDialog.Choice())
+                {
+                    case 1:
+                        //dataManager.SaveDATA();
+                        // Ввод данных
+                        Console.Write("               Input your name -> ");
+                        string name = Console.ReadLine();
+                        Console.Write("               Input your password -> ");
+                        string password = Console.ReadLine();
+                        // Загружает данные про директора 
+                        director = dataManager.LoadDirector();
+                        if (name == director.Name && password == director.Password)
+                        {
+                            do
+                            {
+                                Console.Clear();
+                                directorMenu.LogInMenu();
+                                switch (directorDialog.ChoiceAfter())
+                                {
+                                    case 1:
+                                        //.CreateOrder(order);
+                                        // dataManager.SaveOrder(id, order);
+                                        dataManager.AddManager();
+                                        Console.WriteLine("1");
+                                        break;
+                                    case 2:
+                                        //dataManager.ShowAllOrders(id);
+                                        Console.WriteLine("2");
+
+                                        break;
+                                    case 3:
+                                        //clientMenu.LogOutMenu(client);
+                                        Console.WriteLine("3");
+
+                                        break;
+                                    case 4:
+                                        Console.WriteLine("");
+                                        break;
+                                    case 5:
+                                        exit = true;
+                                        break;
+                                }
+                            } while (exit == false && directorDialog.AllowContinue() == 'y');
+                        }
+                        else
+                        {
+                            safer--;
+                            Console.WriteLine($"\n               Inccorect login or password. {safer} attempts left");
+                        }
+                        break;
+                    case 2:
+                        exit = true;
+                        break;
+                }
+            } while (exit == false && directorDialog.AllowContinue() == 'y');
+            // Finsih ->
+            Console.Clear();
+            directorMenu.Finish();
         }
     }
 }
---
+++ LibraryMenu/DirectorMenu.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library1;
+
+namespace LibraryMenu
+{
+    public class DirectorMenu
+    {
+        private DateTime h = DateTime.Now;
+        public void StartMenu()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("               ╒════════════════════════════════════════════╕");
+            Console.WriteLine("               │         Welcome you to the Happy_PC        │");
+            Console.WriteLine("               │────────────────────────────────────────────│");
+            Console.WriteLine("               │                1. Log in                   │");
+            Console.WriteLine("               │                2. Exit                     │");
+            Console.WriteLine("               ╘════════════════════════════════════════════╛");
+        }
+        public void LogInMenu()
+        {
+            Console.WriteLine("               ╒════════════════════════════════════════════╕");
+           Console.WriteLine($"               |                    Welcome!                |");
+            Console.WriteLine("               │────────────────────────────────────────────│");
+            Console.WriteLine("               │              1. Add manager                │");
+            Console.WriteLine("               │              2. Add repairMan              │");
+            Console.WriteLine("               │              3. Remove manager             │");
+            Console.WriteLine("               │              4. Remove repairMan           │");
+            Console.WriteLine("               │              5. Exit                       │");
+            Console.WriteLine("               ╘════════════════════════════════════════════╛");
+        }
+
+        public void Finish()
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine("               ╒════════════════════════════════════════════╕");
+                if (h.Hour >= 4 && h.Hour < 12)
+                Console.WriteLine($"               |        GoodBye! Have a good morning        | ");
+            else if (h.Hour >= 12 && h.Hour < 17)
+                Console.WriteLine($"               |         GoodBye! Have a good day           | ");
+            else if (h.Hour >= 17 && h.Hour < 24)
+                Console.WriteLine($"               |        GoodBye! Have a good evening        | ");
+            else if (h.Hour >= 0 && h.Hour < 4 || h.Hour == 24)
+                Console.WriteLine($"               |         GoodBye! Have a good night         | ");
+            Console.WriteLine("               ╘════════════════════════════════════════════╛");
+            Console.WriteLine("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+
+        }
+        /*public void AddManagerMenu(Manager manager)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("               ╒════════════════════════════════════════════╕");
+            Console.WriteLine("               │   Okay, now input manager DATA, please     │");
+            Console.WriteLine("               ╘════════════════════════════════════════════╛");
+
+        }*/
+
+    }
+}
